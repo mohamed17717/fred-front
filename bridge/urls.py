e32307d48f03198b7da45072ec455698182ef425
Mo17 <mo.err17@gmail.com>
2020-10-07 18:31:57 +0200

    create all needed api urls

--- bridge/urls.py
+++ bridge/urls.py
@@ -4,10 +4,69 @@ from django.contrib import admin
 from django.urls import path
 
 from contactus.views import redirectMail
-from workaround.views import index
+from workaround.views import (
+    index, 
+
+    getUserRating,
+    setRating,
+    setReview,
+    search,
+    listCourses,
+    listCoursesMostSell,
+    listCoursesMostView,
+    getCourse,
+    buyCourse,
+    viewCourse,
+    filterCourses,
+    getRelatedCourses,
+    listCoaches,
+    listLiveEvents,
+
+    setCoach,
+    deleteCoaches,
+    setLiveEvent,
+    deleteLiveEvents,
+    setCourse,
+    deleteCourses,
+
+    listBlogs,
+    setBlog,
+)
 
 urlpatterns = [
     path('', index),
+    # course links
+    path('rate/<str:courseId>/', setRating),
+    path('rate/<str:courseId>/<str:userId>/', getUserRating),
+    path('review/<str:courseId>/', setReview),
+    path('search/', search),
+    path('courses/', listCourses),
+    path('courses/most-sell/', listCoursesMostSell),
+    path('courses/most-view/', listCoursesMostView),
+    path('course/<str:courseId>/', getCourse),
+    path('buy-course/<str:courseId>/', buyCourse),
+    path('view-course/<str:courseId>/', viewCourse),
+    path('filter/<str:category>/', filterCourses),
+    path('related-courses/<str:courseId>/', getRelatedCourses),
+    # coaches
+    path('coaches/', listCoaches),
+    # live events
+    path('live-events/', listLiveEvents),
+
+    # set coach
+    path('set/course/', setCourse),
+    path('delete/courses/', deleteCourses),
+    
+    path('set/coach/', setCoach),
+    path('delete/coaches/', deleteCoaches),
+    
+    path('set/live-event/', setLiveEvent),
+    path('delete/live-events/', deleteLiveEvents),
+
+    path('set/blog/', setBlog),
+    path('blogs/', listBlogs),
+
+
     path('admin/', admin.site.urls),
     path('contact-us/', redirectMail)
 ]
