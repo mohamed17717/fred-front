7ca65e81e452334fb34c31ee83c22d1e3614f0c7
Mo17 <mo.err17@gmail.com>
2020-10-07 18:32:57 +0200

    show tables in admin

--- workaround/admin.py
+++ workaround/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+  Category,
+  Coach,
+  Course,
+  LiveEvent,
+  Rating, 
+  Review,
+)
+
+admin.site.register(Category)
+admin.site.register(Coach)
+admin.site.register(Course)
+admin.site.register(LiveEvent)
+admin.site.register(Rating)
+admin.site.register(Review)
+
+
+
+
+
+
