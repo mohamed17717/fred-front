2934c3de89372373ce1f59bf9205793e3359baf8
Mo17 <mo.err17@gmail.com>
2020-10-08 04:22:26 +0200

    update static files

--- static_in_env/assets/js/blog.js
+++ static_in_env/assets/js/blog.js
@@ -99,3 +99,60 @@ const footer = (...args) => createElm(`footer`, ...args);
 const h3 = (...args) => createElm(`h3`, ...args);
 const aside = (...args) => createElm(`aside`, ...args);
 
+
+
+// extract data
+document.querySelectorAll('.read-more').forEach(elm => elm.remove())
+
+const posts = document.querySelectorAll('.blog-container .post');
+posts.forEach(post => {
+  let postData = {
+    title: post.querySelector('h1').innerText,
+    author_name: post.querySelector('.post-byline .post-author-name').innerText,
+    author_pp: post.querySelector('.post-byline img').src,
+    date: post.querySelector('.post-byline .post-date').innerText,
+    description: post.querySelector('.post-content').innerText,
+    url: post.querySelector('h1 a').href,
+  }
+
+  const styledPost = 
+    div({class: 'card'},
+      div({class: 'info'}, 
+        div({class: 'title'}, postData.title),
+        p({class: 'overview'}, postData.description),
+        div({class: 'author'}, 
+          div({class: 'profile-pic author2'}),
+          div({class: 'name'}, postData.author_name),
+        ),
+
+        div({class: 'bottom'}, 
+          button({class: 'outline light'}, 'read more'),
+          div({class: 'date'}, postData.date)
+        )
+      )
+    )
+  
+  post.replaceWith(styledPost);
+});
+
+const sidebar = document.querySelector('.sidebar-block')
+sidebar.replaceWith(
+  aside({id: 'blog-sidebar'},
+    div({class: 'search-widget'},
+      div({class: 'search-box'},
+        input({type:"text" ,name:"q" ,id:"search" ,placeholder:"Search..." }),
+        button(i({class: 'icon icon-search'}))
+      )
+    ),
+    div({class: 'recent-posts'},
+      div({class: 'title'}, 'recent posts'),
+      div({class: 'poost'}, 
+        div({class: 'image image1'}),
+        div({class: 'info'}, 
+          div({class: 'name'}, 'Lorem ipsum dolor sit.'),
+          div({class: 'date'}, 'April, 01 2020')
+        )
+      )
+    ),
+  )
+)
