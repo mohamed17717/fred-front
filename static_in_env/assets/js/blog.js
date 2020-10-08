62beae594fbee50d47a2aed15fd37570b31c07d5
Mo17 <mo.err17@gmail.com>
2020-10-08 13:29:09 +0200

    update style

--- static_in_env/assets/js/blog.js
+++ static_in_env/assets/js/blog.js
@@ -12,42 +12,90 @@ posts.forEach(post => {
     url: post.querySelector('h1 a').href,
   }
 
-  const styledPost = 
-    div({class: 'card'},
-      div({class: 'info'}, 
-        div({class: 'title'}, postData.title),
-        p({class: 'overview'}, postData.description),
-        div({class: 'author'}, 
-          div({class: 'profile-pic author2'}),
-          div({class: 'name'}, postData.author_name),
+  const styledPost =
+    div({
+        class: 'card'
+      },
+      div({
+          class: 'info'
+        },
+        div({
+          class: 'title'
+        }, postData.title),
+        p({
+          class: 'overview'
+        }, postData.description),
+        div({
+            class: 'author'
+          },
+          div({
+            class: 'profile-pic author2'
+          }),
+          div({
+            class: 'name'
+          }, postData.author_name),
         ),
 
-        div({class: 'bottom'}, 
-          button({class: 'outline light'}, 'read more'),
-          div({class: 'date'}, postData.date)
+        div({
+            class: 'bottom'
+          },
+          button({
+            class: 'outline light'
+          }, 'read more'),
+          div({
+            class: 'date'
+          }, postData.date)
         )
       )
     )
-  
+
   post.replaceWith(styledPost);
 });
+document.querySelector('.blog-container .col-sm-9').setAttribute('id', 'blog-cards')
 
 const sidebar = document.querySelector('.sidebar-block')
 sidebar.replaceWith(
-  aside({id: 'blog-sidebar'},
-    div({class: 'search-widget'},
-      div({class: 'search-box'},
-        input({type:"text" ,name:"q" ,id:"search" ,placeholder:"Search..." }),
-        button(i({class: 'icon icon-search'}))
+  aside({
+      id: 'blog-sidebar'
+    },
+    div({
+        class: 'search-widget'
+      },
+      div({
+          class: 'search-box'
+        },
+        input({
+          type: "text",
+          name: "q",
+          id: "search",
+          placeholder: "Search..."
+        }),
+        button(i({
+          class: 'icon icon-search'
+        }))
       )
     ),
-    div({class: 'recent-posts'},
-      div({class: 'title'}, 'recent posts'),
-      div({class: 'poost'}, 
-        div({class: 'image image1'}),
-        div({class: 'info'}, 
-          div({class: 'name'}, 'Lorem ipsum dolor sit.'),
-          div({class: 'date'}, 'April, 01 2020')
+    div({
+        class: 'recent-posts'
+      },
+      div({
+        class: 'title'
+      }, 'recent posts'),
+      div({
+          class: 'poost'
+        },
+        div({
+          class: 'image image1'
+        }),
+        div({
+            class: 'info'
+          },
+          div({
+            class: 'name'
+          }, 'Lorem ipsum dolor sit.'),
+          div({
+            class: 'date'
+          }, 'April, 01 2020')
         )
       )
     ),
