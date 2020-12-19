c76bcb00c37c64376eaecfaf6e12f9e000cf08dd
Mo17 <mo.err17@gmail.com>
2020-12-19 13:10:41 +0200

    error in filter

--- workaround/views.py
+++ workaround/views.py
@@ -465,11 +465,12 @@ def deleteCategories(request):
     return HttpResponse(status=200)
 
 @require_http_methods(["POST"])
+@requiredFields(['url', 'description'])
 def setCourseDescription(request):
     url = request.POST.get('url', '').split('/')[-1]
     description = request.POST.get('description', '')
 
-    course = course.objects.filter(url__iendswith=url).first()
+    course = Course.objects.filter(url__iendswith=url).first()
     if not course:
         return HttpResponseNotFound()
 
